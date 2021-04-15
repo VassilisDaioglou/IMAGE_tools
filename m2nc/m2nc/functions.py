987bc8d1f00a31061ab1c387a38d4917b295cf74
VassilisDaioglou <42271684+VassilisDaioglou@users.noreply.github.com>
2021-04-15 13:58:37 +0200

    Cleaned up and streamlined code. Moves reading in of gridfile outside the m2nc class since there is no point of doing it repeatedly. Similarly, moved 'mapping' out of m2nc class, for same reason

--- m2nc/m2nc/functions.py
+++ m2nc/m2nc/functions.py
@@ -20,28 +20,16 @@ class m2nc:
     map from a vector into a grid, and subsequently outputs it, including
     metadata into a netCDF map.
     """
-    def __init__(self, mmap_in, map_title, map_var, map_unit, map_outname, timexist):
+    def __init__(self, mmap_in, map_title, map_var, map_unit, map_outname, timexist, mapping):
         self.mmap_in = mmap_in
         self.map_title = map_title
         self.map_var = map_var
         self.map_unit = map_unit
         self.map_outname = map_outname
         self.timexist = timexist
+        self.mapping = mapping
 
     def run_m2nc(self):
-        """
-        Function which starts procedure.
-        Reads in ancilliary data needed to convert m-map to grids. 
-            - text file linking m-map verctor to coordinates
-            - Generic map used to apply masking of oceans etc.
-        """
-        # *** DATA NEEDED TO GET THE m-to-netCDF MAPPING ***
-        self.grdfile  = np.loadtxt(InputDir.data_dir + 'mcoord.txt')
-        
-        # Produce NC maps
-        self.make_nc_map()
-
-    def make_nc_map(self):
         """
         Procedure which m-maps (vectors) to to grids and subsequently to netCDF maps
 
@@ -52,68 +40,21 @@ class m2nc:
 
         Third: Output this grid as a netCDF file  
         """
+        print("\tReading in m-map")
         if self.timexist:
             self.mmap, self.time = read_mym(self.mmap_in, path= InputDir.in_dir)
         else:
             self.mmap = read_mym(self.mmap_in, path= InputDir.in_dir)
 
         # Create map linking m-maps to lat/lon matrix
-        mapping = self.get_mmapping()
-        gridmap = self.get_gridmap(self.mmap, mapping, self.timexist)
+        print("\tCreating gridded map")
+        gridmap = self.get_gridmap(self.mmap, self.mapping, self.timexist)
         
         # *** WRITE OUTPUT ***
+        print("\tWriting netCDF output")
         writemap = WriteMaps(gridmap, self.map_title, self.map_var, self.map_unit, self.map_outname)
         writemap.maptime2nc() if self.timexist else writemap.map2nc()
     
-    def read_map(self, file_name, var_name, type='float32', maskvalue=-9999):
-        """
-        Return an array with the values of a variable in a netCDF map
-        """
-        nc_map = netCDF4.Dataset(InputDir.data_dir + file_name)
-        var_array = self.extract_map(nc_map, var_name, type, maskvalue)
-        return var_array
-
-    def extract_map(self, file, variable, type='float32', maskvalue=-9999):
-        """
-        Returns a numpy array of the variable in a netCDF file
-        Inputs:
-        1. netCDF File
-        2. Variable name
-        3. Data type of output. float64 by default
-        4. Mask value: -9999 by default
-
-        Output:
-        Masked numpy array of correct dtype and mask fill_value
-
-        Comment:
-        Have to store a pre-corrected version of the map
-        because the fill_value changes during operations.
-        This is a numpy bug, see: https://github.com/numpy/numpy/issues/3762
-        """
-        varin = file.variables[variable][:]
-        varout = np.array(varin, dtype=type)
-        varout = np.ma.masked_where(varin == varin.fill_value, varout)
-        varout = ma.masked_values(varout, maskvalue)
-        return varout
-
-    def get_mmapping(self): 
-        """
-        Returns a matrix which maps the rows of m-maps 
-        on a grid of latitude/longitude
-        """
-        nlats = 360
-        nlons = 720
-        lats = np.zeros(nlats)
-        lons = np.zeros(nlons)
-        lats[:] = 90. - (180./nlats)*np.arange(nlats) # north pole to south pole
-        lons[:] = -180. + (180./nlats)*np.arange(nlons) # 180degree longitude eastward
-
-        mapping = []
-        for i in range(len(self.grdfile)):   # per row
-            loc_lon = np.where(lons == self.grdfile[i,0])
-            loc_lat = np.where(lats == self.grdfile[i,1])
-            mapping.append([loc_lat[0].astype(np.int32), loc_lon[0].astype(np.int32)])
-        return mapping
 
     def get_gridmap(self, mmap, mapping, existtime):
         """
@@ -130,9 +71,6 @@ class m2nc:
                     map_loc = mapping[i]
                     gridmap[t,map_loc[0], map_loc[1]] = mmap[t][i]
                 
-                # Using list comprehension (THIS IS SLOWER!!)
-                #[gridmap[t, mmap[t][np.where(mapping == i)[0]].astype(np.int32), mmap[t][np.where(mapping == i)[1]].astype(np.int32)] for i in range(len(mmap[0]))]
-                
             # Incase timesteps are missing:
             if len(gridmap) < 27:
                 newtsteps = 27 - len(gridmap)
@@ -146,3 +84,22 @@ class m2nc:
                 gridmap[map_loc[0],map_loc[1]] = mmap[0][i]
 
         return gridmap
+
+def get_mmapping(grdfile): 
+    """
+    Returns a matrix which maps the rows of m-maps 
+    on a grid of latitude/longitude
+    """
+    nlats = 360
+    nlons = 720
+    lats = np.zeros(nlats)
+    lons = np.zeros(nlons)
+    lats[:] = 90. - (180./nlats)*np.arange(nlats) # north pole to south pole
+    lons[:] = -180. + (180./nlats)*np.arange(nlons) # 180degree longitude eastward
+
+    mapping = []
+    for i in range(len(grdfile)):   # per row
+        loc_lon = np.where(lons == grdfile[i,0])
+        loc_lat = np.where(lats == grdfile[i,1])
+        mapping.append([loc_lat[0].astype(np.int32), loc_lon[0].astype(np.int32)])
+    return mapping
--- m2nc/m2nc/m2nc.py
+++ m2nc/m2nc/m2nc.py
@@ -4,13 +4,15 @@ Script to convert m-maps to netCDF maps
 Author: Vassilis Daioglou
 Date: April 2021
 """
-from functions import m2nc
+import numpy as np
+from functions import m2nc, get_mmapping
+from dirs import InputDir
 
 # Create list of maps to be outputted with the following information:
     # 1. m-map file name, 2. Map Title, 3. Variable Name, 4. Unit, 5. Output Name, 6. Has time dimension (boolean)
         
 output_maps_list = [
-            ['EFMapType2_t.out','Non-Woody Gradual Emission Factor','NWOOD_EF_Gradual','kgCO2/km^2','NWOOD_EF_Gradual_t',True],
+            #['EFMapType2_t.out','Non-Woody Gradual Emission Factor','NWOOD_EF_Gradual','kgCO2/km^2','NWOOD_EF_Gradual_t',True],
             ['nw_EmisType1_pkm.out','Instantaneous Emission Factor','EF_Instant','kgCO2/km^2','EF_Instant',False],
             ['nw_EmisType2ST_pkm.out','Non-Woody Gradual Emission Factor','NWOOD_EF_Gradual','kgCO2/km^2','NWOOD_EF_Gradual',False],
             ['wd_EmisType2ST_pkm.out','Woody Gradual Emission Factor','WOODY_EF_Gradual','kgCO2/km^2','WOODY_EF_Gradual',False],
@@ -19,9 +21,11 @@ output_maps_list = [
             ['oc_EmisType2ST_pkm.out','Oilcrops Gradual Emission Factor','OILCR_EF_Gradual','kgCO2/km^2','OILCR_EF_Gradual',False],
             ]
             
- # TODO Read grid coordinates here, instead of during every loop.
- # Also, it seems like row 5 of grid coordinates are also read in....shouldnt!
+# File identifying grid coordinates of m-map, needed to create m-2-grid mapping
+grdfile  = np.loadtxt(InputDir.data_dir + 'mcoord.txt')
+mapping = get_mmapping(grdfile)
 
 for outmap in output_maps_list:
-    write_output = m2nc(outmap[0], outmap[1], outmap[2], outmap[3], outmap[4], outmap[5])
+    print("\nProcessing Map: ", outmap[1])
+    write_output = m2nc(outmap[0], outmap[1], outmap[2], outmap[3], outmap[4], outmap[5], mapping)
     write_output.run_m2nc() 
