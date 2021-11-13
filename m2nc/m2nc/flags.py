1e8f5b7f3b09011fdc861309eaa171ce6a443a8d
VassilisDaioglou <42271684+VassilisDaioglou@users.noreply.github.com>
2021-11-13 13:31:48 +0100

    Modularised functions. Now flags.py states which functions are to be run and entire module operates within a 'main' definition

---
+++ m2nc/m2nc/flags.py
@@ -0,0 +1,14 @@
+class RunFunction:
+    """
+    Flags which turn on or off specific functions
+
+    m2nc: Converts m-maps to netCDF
+    nc2m: Converts netCDF maps to m-maps
+    list2m: Converts listed data (i.e. country data in excel) to m-map
+    list2nc: Converts listed data (i.e. country data in excel) to netCDF
+    """
+    def __init__(self, m2nc=False, nc2m=True, list2m=False, list2nc=False):
+        self.m2nc = m2nc
+        self.nc2m = nc2m
+        self.list2m = list2m
+        self.list2nc = list2nc
